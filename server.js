3250534a9b35d8e59f4e5e2347ff1d945cba4320
Soleiles <ktaing789@gmail.com>
2023-05-31 20:01:35 -0400

    working directory

--- server.js
+++ server.js
@@ -6,7 +6,16 @@ const path = require('path');
 const PORT = 3001;
 const app = express();
 
+app.use(express.static('public'));
 
+// ****** GET homepage ****** //
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+// ****** GET notes page ****** //
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`App listening at http://localhost:${PORT}`)
